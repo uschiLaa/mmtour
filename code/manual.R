fcafd318b20538687a11ac050c0791e07757b251
dicook <visnut@gmail.com>
2022-04-01 16:24:48 +1100

    colour of selected variable

--- code/manual.R
+++ code/manual.R
@@ -12,7 +12,8 @@
 
 
 # Utility function to plot a basis
-plot_basis <- function(A) {
+# Need to generalise to arbitrary pxd
+plot_basis <- function(A, v=NULL) {
   # Set plot canvas
   plot(c(-1.1, 1.1), c(-1.1, 1.1),
        type="n", xlab="", ylab="", axes=FALSE)
@@ -22,8 +23,15 @@ plot_basis <- function(A) {
 
   # label axes
   text(A[,1], A[,2], rownames(A), col="grey50")
+  if (!is.null(v))
+    text(A[v,1], A[v,2], rownames(A), col="orange")
+
 
   # draw axes
   A <- cbind(A, xctr=rep(0, nrow(A)), yctr=rep(0, nrow(A)))
   segments(A[,3], A[,4], A[,1], A[,2], lwd=3)
+  if (!is.null(v))
+    segments(A[v,3], A[v,4], A[v,1], A[v,2],
+             col="orange", lwd=3)
+
 }
